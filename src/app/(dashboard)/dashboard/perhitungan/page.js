f118f8b603208e7fe8bbb42bbc463f2423cd80f9
bagas aditya <bagasaditya599@gmail.com>
2026-01-27 01:08:24 +0700

    feat: implement detergent calculation logic and enhance form handling in PerhitunganDetergenPage; update button component formatting

--- src/app/(dashboard)/dashboard/perhitungan/page.js
+++ src/app/(dashboard)/dashboard/perhitungan/page.js
@@ -3,16 +3,78 @@ import { AccordionMultiple } from "@/components/accordion";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 export default function PerhitunganDetergenPage() {
+  const [beratPakaian, setBeratPakaian] = useState("");
+  const [tingkatKotor, setTingkatKotor] = useState("");
+  const [warnaPakaian, setWarnaPakaian] = useState("");
+  const [hasil, setHasil] = useState();
+  function muBerat(x) {
+    if (x <= 5) return 0;
+    if (x >= 10) return 1;
+    return (x - 5) / 5;
+  }
+
+  function muSedang(x) {
+    if (x <= 5) return 1;
+    if (x >= 10) return 0;
+    return (10 - x) / 5;
+  }
+
+  function muKotorTinggi(x) {
+    if (x <= 5) return 0;
+    if (x >= 10) return 1;
+    return (x - 5) / 5;
+  }
+
+  function zDeterjen(alpha) {
+    return 20 + alpha * 80;
+  }
+
+  function hitungDetergen(berat, kotor) {
+    const beratBerat = muBerat(berat);
+    const beratSedang = muSedang(berat);
+    const kotorTinggi = muKotorTinggi(kotor);
+
+    const alpha1 = Math.min(beratBerat, kotorTinggi);
+    const alpha2 = Math.min(beratSedang, kotorTinggi);
+
+    const z1 = zDeterjen(alpha1);
+    const z2 = zDeterjen(alpha2);
+
+    const Z = (alpha1 * z1 + alpha2 * z2) / (alpha1 + alpha2);
+    console.log(
+      "Z: ",
+      z1,
+      z2,
+      alpha1,
+      alpha2,
+      beratBerat,
+      beratSedang,
+      kotorTinggi,
+    );
+    return Z.toFixed(2);
+  }
+
+  const handleHitung = (e) => {
+    e.preventDefault(); // penting biar form gak reload
+
+    const berat = Number(beratPakaian);
+    const kotor = Number(tingkatKotor);
+    console.log(berat, kotor);
+    const hasilPerhitungan = hitungDetergen(berat, kotor);
+    console.log(hasilPerhitungan);
+    setHasil(hasilPerhitungan);
+  };
+
+  console.log(hasil);
   return (
     <div className="">
       <h1>Perhitungan Detergen</h1>
@@ -22,48 +84,63 @@ export default function PerhitunganDetergenPage() {
           <CardTitle>Form Perhitungan Detergen</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleHitung}>
             <div className="flex flex-col gap-6">
-              <div className=" flex">
-                <Label
-                  htmlFor="beratPakaian"
-                  className="w-fit text-nowrap mr-2"
-                >
+              <div className="flex">
+                <Label htmlFor="beratPakaian" className="w-fit mr-2">
                   Berat Pakaian (kg)
                 </Label>
-                <Input id="beratPakaian" type="number" required />
+                <Input
+                  id="beratPakaian"
+                  type="number"
+                  value={beratPakaian}
+                  onChange={(e) => setBeratPakaian(e.target.value)}
+                  required
+                />
               </div>
-              <div className="grid gap-2">
-                <div className="flex">
-                  <Label
-                    htmlFor="tingkatKotor"
-                    className="w-fit mr-2 text-nowrap"
-                  >
-                    Tingkat Kotor
-                  </Label>
-                  <Input id="tingkatKotor" type="number" required />
-                </div>
+
+              <div className="flex">
+                <Label htmlFor="tingkatKotor" className="w-fit mr-2">
+                  Tingkat Kotor
+                </Label>
+                <Input
+                  id="tingkatKotor"
+                  type="number"
+                  value={tingkatKotor}
+                  onChange={(e) => setTingkatKotor(e.target.value)}
+                  required
+                />
               </div>
-              <div className="grid gap-2">
-                <div className="flex">
-                  <Label
-                    htmlFor="warnaPakaian"
-                    className="w-fit mr-2 text-nowrap"
-                  >
-                    Warna Pakaian
-                  </Label>
-                  <Input id="warnaPakaian" type="text" required />
-                </div>
+
+              <div className="flex">
+                <Label htmlFor="warnaPakaian" className="w-fit mr-2">
+                  Warna Pakaian
+                </Label>
+                <Input
+                  id="warnaPakaian"
+                  type="text"
+                  value={warnaPakaian}
+                  onChange={(e) => setWarnaPakaian(e.target.value)}
+                  required
+                />
               </div>
             </div>
           </form>
         </CardContent>
+
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full" onClick={handleHitung}>
             Hitung Takaran Detergen
           </Button>
         </CardFooter>
       </Card>
+
+      {hasil && (
+        <div className="mt-4 text-center">
+          <p className="text-sm text-muted-foreground">Takaran Detergen</p>
+          <p className="text-2xl font-bold">{hasil} ml</p>
+        </div>
+      )}
 
       <Card className="w-full my-2">
         <CardHeader>
